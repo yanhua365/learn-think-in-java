3fadc1dfff4a909f2d5e549e3dfa5501503ef92a
yanhua365 <yanhua365@gmail.com>
2013-10-02 20:16:59 +0800

    初始化工程

--- src/test/java/com/github/yanhua365/learn/thinkinjava/io/DirLisTest.java
+++ src/test/java/com/github/yanhua365/learn/thinkinjava/io/DirLisTest.java
@@ -11,6 +11,7 @@ import static org.junit.Assert.assertThat;
 public class DirLisTest {
     @Test
     public void canListFileInCurrentDir() {
+        //可以列出当前目录下的文件
         List<String> list = Arrays.asList(new DirList().list());
        assertThat(list,hasItem("build.gradle"));
     }
